5730af99b0f216ab9553ba0d5574060d2d062521
Lucas Capoani <lucascapoani@hotmail.com>
2025-06-23 18:10:18 -0300

    Iniciando projeto com tabela e formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { TabelaProdutosComponent } from './tabela-produtos/tabela-produtos.component';
+import { FormProdutosComponent } from './form-produtos/form-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    TabelaProdutosComponent,
+    FormProdutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
